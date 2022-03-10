07f98777cac2d4c1beb7a69e6268ee0ea7c19044
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-10 07:59:20 +0600

    music

---
+++ script.js
@@ -0,0 +1,78 @@
+const elementById = (id) => {
+  return document.getElementById(id);
+};
+
+const handleSearch = () => {
+  const keyword = elementById("keyword");
+  const artistContainer = elementById("artists");
+  const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showArtists(data));
+  artistContainer.innerHTML = ''
+  keyword.value = ''
+
+  // error handled when click on the album and search other band name(ager album gula ashbe na)
+  const albumContainer = elementById("albums");
+  albumContainer.innerHTML = ''
+};
+
+const showArtists = (data) => {
+
+  const artistContainer = elementById("artists");
+  data?.artists?.forEach((artist) => {
+    const div = document.createElement("div");
+    div.classList.add("artist-card");
+    div.innerHTML = `<div class="image-container">
+    <div class="image-container-inner">
+      <img
+        src="${artist.strArtistThumb ? artist.strArtistThumb : 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'}"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="info-container">
+    <h1>${artist.strArtist}</h1>
+    <p>Country: ${artist.strCountry}</p>
+    <p>Style: ${artist.strGenre}</p>
+  </div>
+  <button class="album-button">
+    <i class="fa-solid fa-compact-disc"></i>
+    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+  </button>`;
+    artistContainer.appendChild(div);
+  });
+};
+
+const fetchAlbums = (id) => {
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAlbum(data.album));
+  const artistContainer = elementById("artists");
+  artistContainer.innerHTML = "";
+
+};
+
+const showAlbum = (data) => {
+  console.log(data)
+  const albumContainer = elementById("albums");
+  data.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("album");
+    div.innerHTML = `
+        <div class="album-image-container">
+          <img
+            src="${item.strAlbumThumb ? item.strAlbumThumb : 'https://images.macrumors.com/t/vMbr05RQ60tz7V_zS5UEO9SbGR0=/1600x900/smart/article-new/2018/05/apple-music-note.jpg'}"
+            alt=""
+          />
+        </div>
+        <div class="album-name">
+          <h3>${item.strAlbum}</h3>
+        </div>
+      `;
+
+    albumContainer.appendChild(div);
+
+  });
+};
